71afa8edc8ea56f954b1402632150469d5071f91
Silas Snider <swsnider@gmail.com>
2012-11-04 21:08:30 -0800

    more setup.py fixes

--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
-import ez_setup
-ez_setup.use_setuptools()
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-  return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
   name = "ccp_sde_parser",
-  version = "1.0.0",
+  version = "1.0.1",
   author = "Silas Snider",
   author_email = "swsnider@gmail.com",
   description = ("EVE Online Static Dump Export Parser. Works with https://github.com/eve-val/dump_extractor"),
@@ -20,7 +11,7 @@ setup(
   keywords = "EVE Online Static Dump Export Parser",
   url = "https://github.com/eve-val/dump-parser",
   packages=['ccp_sde_parser'],
-  long_description=read('README.md'),
+  long_description="EVE Online Static Dump Export Parser. Works with https://github.com/eve-val/dump_extractor",
   classifiers=[
     "Development Status :: 3 - Alpha",
     "Topic :: Utilities",
@@ -28,7 +19,7 @@ setup(
   ],
   install_requires=[
     'google-api-python-client',
-    'gflags',
+    'python-gflags',
     'progressbar',
     'PyYAML'
   ],
@@ -39,7 +30,7 @@ setup(
       'merge_ccp_yaml = ccp_sde_parser.merge_ccp_yaml:main',
       'csvize = ccp_sde_parser.csvize:main',
       'sqlize = ccp_sde_parser.sqlize:main',
-      'biqueryize = ccp_sde_parser.biqueryize:main',
+      'bigqueryize = ccp_sde_parser.bigqueryize:main',
     ]
   }
 )
